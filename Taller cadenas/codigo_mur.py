bc90d4f0ee5e6439e3cad59b72bb4a2560762abc
zeroxc23 <116192239+zeroxc23@users.noreply.github.com>
2023-02-20 08:03:42 -0500

    Update codigo_mur.py

--- Taller cadenas/codigo_mur.py
+++ Taller cadenas/codigo_mur.py
@@ -1,12 +1,15 @@
 #Invente un cifrado de texto tipo murcielago o César. Puede utilizar alguna formula matemática para este fin.
 def codi(a):
-    cod=['m','u','r','c','i','e','l','a','g','o']
-    print('murcielago')
-    print('0123456789')
+    cod=['m','u','r','c','i','e','l','a','g','o'] 
+    cod1=["M","U","R","C","I","E","L","A","G","O"]
+    print("Referencia",'murcielago')
+    print("Referencia",'0123456789')
     sad=''
     for i in range(len(a)):
         if a[i] in cod:
             sad+=str(cod.index(a[i]))
+        elif a[i] in cod1:
+            sad+=str(cod1.index(a[i]))
         else:
             sad+=a[i]
     print(sad)            
