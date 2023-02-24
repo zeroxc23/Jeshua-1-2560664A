621c92e623e18e3ed6a742346cd414f6e9748d15
zeroxc23 <116192239+zeroxc23@users.noreply.github.com>
2023-02-24 10:59:57 -0500

    Update Excep-4.py

--- Excepciones integradas/Excep-4.py
+++ Excepciones integradas/Excep-4.py
@@ -3,9 +3,9 @@ def neg():
         num=int(input("Ingrese numero positivo o negativo:"))
         print(f" El {num} fue seleccionado")
         if num>=0:
-            print("Numero positivo")
+            print(f"{num}Es numero positivo")
         else:
-            print("Numero negativo")
+            print(f"{num}Es numero negativo")
     except ValueError as c:
         print(c)
         print("No es un valor numerico intente de nuevo")
