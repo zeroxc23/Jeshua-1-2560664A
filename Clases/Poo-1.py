c4e0ee96a5c30a91accd85ea064819730db5b273
zeroxc23 <116192239+zeroxc23@users.noreply.github.com>
2023-03-01 08:04:17 -0500

    Update Poo-1.py

--- Clases/Poo-1.py
+++ Clases/Poo-1.py
@@ -3,10 +3,10 @@ class Persona:                            #Se define o se crea la clase perosna
         self.__nombre=nombre              #Se define el atributo relacionando con el parametro nombre
         #print('Constructor Activado')        
 
-    def getNombre(self):                  #Se define la función getnombre
+    def getNombre(self):                  #Se define el metodo getnombre
         return self.__nombre              #Devuelve el atributo nombre para saber el nombre incluso estando privado
 
-    def setNombre(self,nombre):           #Se define la función setnombre con el parametro nombre
+    def setNombre(self,nombre):           #Se define el metodo setnombre con el parametro nombre
         self.__nombre=nombre              #Se relaciona el atributo con el parametro para agregar otor nombre
 
 ob=Persona('Maria')                       #Determina el objeto con su determinada clase
